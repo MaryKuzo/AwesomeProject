2a01f9ab6b7337199fccd608952214f1dccf0b70
MaryKuzo <124557630+MaryKuzo@users.noreply.github.com>
2023-10-10 09:08:55 +0300

    ProfileScreen

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -1,76 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
   View,
-  Image,
-  TouchableOpacity,
   StyleSheet,
   ImageBackground,
   Text,
-  FlatList,
   SafeAreaView,
-  RefreshControl,
 } from "react-native";
-import { AntDesign, Feather, Ionicons } from "@expo/vector-icons";
-import { useFocusEffect, useNavigation } from "@react-navigation/native";
+
 import UserPhoto from "../components/UserPhoto";
 import { useDispatch } from "react-redux";
-import { usePost, useUser } from "../hooks/index.js";
-import {
-  addLikeThunk,
-  deleteLikeThunk,
-  getMyPostThunk,
-} from "../redux/posts/postOperations";
-import { auth } from "../redux/config";
-import { updatePage } from "../helpers/index";
+import { useUser } from "../hooks/index.js";
+
 import ButtonLogOut from "../components/ButtonLogOut";
-import PostForm from "../components/PostForm";
+import ProfilePosts from '../components/ProfilePosts.jsx'
 
 const ProfileScreen = () => {
   const [update, setUpdate] = useState(false);
   const { user } = useUser();
   const dispatch = useDispatch();
-  const navigation = useNavigation();
-  const { myPosts } = usePost();
-
-  useEffect(() => {
-    dispatch(getMyPostThunk());
-  }, [dispatch]);
-
-  const fetchMyPosts = async () => {
-    setUpdate(true);
-    try {
-      await dispatch(getMyPostThunk());
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-    }
-    setUpdate(false);
-  };
-
-  const addLike = async (id, likes) => {
-    const value = (likes += 1);
-    dispatch(addLikeThunk({ id, value }));
-
-    updatePage(id, () => {
-      dispatch(getMyPostThunk());
-    });
-  };
-
-  const deleteLike = async (id, likes) => {
-    const value = (likes -= 1);
-    dispatch(deleteLikeThunk({ id, value }));
-
-    updatePage(id, () => {
-      dispatch(getMyPostThunk());
-    });
-  };
-
-  const uid = auth.currentUser ? auth.currentUser.uid : null;
-
-  useFocusEffect(
-    React.useCallback(() => {
-      fetchMyPosts();
-    }, [])
-  );
 
   return (
     <View style={styles.container}>
@@ -85,7 +32,7 @@ const ProfileScreen = () => {
               <ButtonLogOut />
             </View>
             {user && <Text style={styles.name}>{user.displayName}</Text>}
-            <PostForm/>
+            <ProfilePosts/>
           </View>
         </SafeAreaView>
       </ImageBackground>
@@ -131,98 +78,7 @@ const styles = StyleSheet.create({
     marginBottom: 33,
     marginTop: -52,
   },
-  publicationsContainer: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "flex-start",
-  },
-  publicationContainer: {
-    width: "100%",
-    marginBottom: 23,
-  },
-  photo: {
-    width: "100%",
-    height: 240,
-    borderRadius: 8,
-    marginBottom: 10,
-  },
-  publicationName: {
-    color: "#212121",
-    fontFamily: "Roboto_Regular",
-    fontSize: 16,
-    fontStyle: "normal",
-    fontWeight: "500",
-    marginBottom: 10,
-  },
-  publicationDataContainer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    gap: 35,
-  },
-  publicationIconContainer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    gap: 24,
-  },
-  publicationCommentContainer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    gap: 6,
-    alignItems: "center",
-  },
-  count: {
-    color: "#212121",
-    fontFamily: "Roboto_Regular",
-    fontSize: 16,
-    fontStyle: "normal",
-    fontWeight: "400",
-  },
-  iconGray: {
-    color: "#BDBDBD",
-  },
-  countZero: {
-    color: "#BDBDBD",
-  },
-  publicationLikeContainer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    gap: 6,
-    alignItems: "center",
-  },
-  icon: {
-    color: "#FF6C00",
-  },
-  likeCount: {
-    color: "#212121",
-    fontFamily: "Roboto_Regular",
-    fontSize: 16,
-    fontStyle: "normal",
-    fontWeight: "400",
-  },
-  publicationLocationContainer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    flexShrink: 1,
-    alignItems: "center",
-  },
-  iconLocation: {
-    color: "#BDBDBD",
-  },
-  location: {
-    color: "#212121",
-    textAlign: "right",
-    fontFamily: "Roboto_Regular",
-    fontSize: 16,
-    fontStyle: "normal",
-    fontWeight: "400",
-    textDecorationLine: "underline",
-    flexShrink: 1,
-  },
+  
 });
 
 export default ProfileScreen;
---
+++ components/ProfilePosts.jsx
@@ -0,0 +1,320 @@
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+  Text,
+  FlatList,
+  SafeAreaView,
+  RefreshControl,
+} from "react-native";
+import { AntDesign, Feather, Ionicons } from "@expo/vector-icons";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import UserPhoto from "../components/UserPhoto";
+import { useDispatch } from "react-redux";
+import { usePost, useUser } from "../hooks/index.js";
+import {
+  addLikeThunk,
+  deleteLikeThunk,
+  getMyPostThunk,
+} from "../redux/posts/postOperations";
+import { auth } from "../redux/config";
+import { updatePage } from "../helpers/index";
+import ButtonLogOut from "../components/ButtonLogOut";
+
+
+const ProfilePosts = () => {
+  const [update, setUpdate] = useState(false);
+  const { user } = useUser();
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+  const { myPosts } = usePost();
+
+  useEffect(() => {
+    dispatch(getMyPostThunk());
+  }, [dispatch]);
+
+  const fetchMyPosts = async () => {
+    setUpdate(true);
+    try {
+      await dispatch(getMyPostThunk());
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+    setUpdate(false);
+  };
+
+  const addLike = async (id, likes) => {
+    const value = likes + 1;
+    dispatch(addLikeThunk({ id, value }));
+
+    updatePage(id, fetchMyPosts);
+  };
+
+  const deleteLike = async (id, likes) => {
+    const value = likes - 1;
+    dispatch(deleteLikeThunk({ id, value }));
+
+    updatePage(id, fetchMyPosts);
+  };
+
+  const uid = auth.currentUser ? auth.currentUser.uid : null;
+
+  useFocusEffect(
+    React.useCallback(() => {
+      fetchMyPosts();
+    }, [])
+  );
+
+  return (
+            <FlatList
+              refreshControl={
+                <RefreshControl refreshing={update} onRefresh={fetchMyPosts} />
+              }
+              data={myPosts}
+              keyExtractor={(item) => item.id}
+              showsVerticalScrollIndicator={false}
+              renderItem={({ item }) => (
+                <View style={styles.publicationsContainer}>
+                  <View style={styles.publicationContainer} key={item.id}>
+                    {item.imageURL && (
+                      <Image
+                        source={{ uri: item.imageURL }}
+                        style={styles.photo}
+                      />
+                    )}
+                    <Text style={styles.publicationName}>{item.name}</Text>
+                    <View style={styles.publicationDataContainer}>
+                      <View style={styles.publicationIconContainer}>
+                        {item.comments.some(
+                          (commentsItem) => commentsItem.userId === uid
+                        ) ? (
+                          <>
+                            <TouchableOpacity
+                              onPress={() => {
+                                navigation.navigate("Comments", {
+                                  post: item,
+                                });
+                              }}
+                              style={styles.publicationCommentContainer}
+                            >
+                              <Ionicons
+                                name="chatbubble"
+                                size={24}
+                                style={styles.icon}
+                              />
+                              <Text style={styles.count}>
+                                {item.comments.length}
+                              </Text>
+                            </TouchableOpacity>
+                          </>
+                        ) : (
+                          <>
+                            <TouchableOpacity
+                              onPress={() => {
+                                navigation.navigate("Comments", {
+                                  post: item,
+                                });
+                              }}
+                              style={styles.publicationCommentContainer}
+                            >
+                              <Ionicons
+                                name="chatbubble-outline"
+                                size={24}
+                                style={styles.iconGray}
+                              />
+                              <Text style={styles.countZero}>
+                                {item.comments.length}
+                              </Text>
+                            </TouchableOpacity>
+                          </>
+                        )}
+                        {item.liked.some(
+                          (likedItem) => likedItem.userId === uid
+                        ) ? (
+                          <TouchableOpacity
+                            onPress={() => deleteLike(item.id, item.likes)}
+                            style={styles.publicationLikeContainer}
+                          >
+                            <Feather
+                              name="thumbs-up"
+                              size={24}
+                              style={styles.icon}
+                            />
+                            <Text style={styles.count}>{item.likes}</Text>
+                          </TouchableOpacity>
+                        ) : (
+                          <TouchableOpacity
+                            onPress={() => addLike(item.id, item.likes)}
+                            style={styles.publicationLikeContainer}
+                          >
+                            <Feather
+                              name="thumbs-up"
+                              size={24}
+                              style={styles.iconGray}
+                            />
+                            <Text style={[styles.count, styles.countZero]}>
+                              {item.likes}
+                            </Text>
+                          </TouchableOpacity>
+                        )}
+                      </View>
+                      <TouchableOpacity
+                        style={styles.publicationLocationContainer}
+                        onPress={() => {
+                          navigation.navigate("Map");
+                        }}
+                      >
+                        <AntDesign
+                          name="enviromento"
+                          size={24}
+                          style={styles.iconLocation}
+                        />
+                        <Text style={styles.location}>
+                          {item.location}, {item.geolocation}
+                        </Text>
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                </View>
+              )}
+            />
+
+  );
+};
+export default ProfilePosts;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: "relative",
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+  },
+  safeContainer: {
+    flex: 1,
+  },
+  profileContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    position: "relative",
+    marginTop: 147,
+    paddingHorizontal: 16,
+    backgroundColor: "#FFF",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  containerLogOut: {
+    alignSelf: "flex-end",
+    top: -102,
+  },
+  name: {
+    color: "#212121",
+    textAlign: "center",
+    fontFamily: "Roboto_Bold",
+    fontSize: 30,
+    fontStyle: "normal",
+    fontWeight: "500",
+    letterSpacing: 0.3,
+    marginBottom: 33,
+    marginTop: -52,
+  },
+  publicationsContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+  },
+  publicationContainer: {
+    width: "100%",
+    marginBottom: 23,
+  },
+  photo: {
+    width: "100%",
+    height: 240,
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+  publicationName: {
+    color: "#212121",
+    fontFamily: "Roboto_Regular",
+    fontSize: 16,
+    fontStyle: "normal",
+    fontWeight: "500",
+    marginBottom: 10,
+  },
+  publicationDataContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: 35,
+  },
+  publicationIconContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    gap: 24,
+  },
+  publicationCommentContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    gap: 6,
+    alignItems: "center",
+  },
+  count: {
+    color: "#212121",
+    fontFamily: "Roboto_Regular",
+    fontSize: 16,
+    fontStyle: "normal",
+    fontWeight: "400",
+  },
+  iconGray: {
+    color: "#BDBDBD",
+  },
+  countZero: {
+    color: "#BDBDBD",
+  },
+  publicationLikeContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    gap: 6,
+    alignItems: "center",
+  },
+  icon: {
+    color: "#FF6C00",
+  },
+  likeCount: {
+    color: "#212121",
+    fontFamily: "Roboto_Regular",
+    fontSize: 16,
+    fontStyle: "normal",
+    fontWeight: "400",
+  },
+  publicationLocationContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    flexShrink: 1,
+    alignItems: "center",
+  },
+  iconLocation: {
+    color: "#BDBDBD",
+  },
+  location: {
+    color: "#212121",
+    textAlign: "right",
+    fontFamily: "Roboto_Regular",
+    fontSize: 16,
+    fontStyle: "normal",
+    fontWeight: "400",
+    textDecorationLine: "underline",
+    flexShrink: 1,
+  },
+});
+
+
